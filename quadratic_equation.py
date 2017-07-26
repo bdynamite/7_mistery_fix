249dd9ec38ebca3f5d06822a595a8fa61777d932
bdynamite <bdynamite@yandex.ru>
2017-07-26 12:54:57 +0300

    commit#1

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,29 @@
 from math import sqrt
 
 
+def get_params():
+    params = input('Enter the coefficients of the quadratic equation: ').split()
+    try:
+        params = list(map(int, params))
+    except ValueError:
+        print('Coefficients must be integer')
+        exit()
+    if len(params) != 3:
+        print('There should be 3 coefficients ')
+        exit()
+    return params
+
+
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return root1, None
+    if discriminant < 0:
+        return None, None
+    elif discriminant == 0:
+        return (-b) / (2 * a), None
     else:
-        return root1, root2
+        return (-b - sqrt(discriminant)) / (2 * a), (-b + sqrt(discriminant)) / (2 * a)
+
+
+if __name__ == '__main__':
+    params = get_params()
+    print(get_roots(*params))   
